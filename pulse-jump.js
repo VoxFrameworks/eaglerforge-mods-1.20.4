465c1499645609835e8e12ba84716da5e51ede49
VoxFrameworks <vdmofficially@outlook.com>
2026-02-13 16:52:26 -0500

    Prevent fall damage when jumping with key 'h'

--- pulse-jump.js
+++ pulse-jump.js
@@ -3,7 +3,9 @@ ModAPI.require("network"); // for fall damage prevention
 ModAPI.addEventListener("key", (event) => {
   if (event.key === 35) { // Key code for 'h'
     ModAPI.player.motionY += 1;
+  if (ModAPI.player.fallDistance > 2.0) {
       ModAPI.network.sendPacketPlayer({isOnGround: true}); // prevent fall damage
+  } 
     ModAPI.updateComponent("player");
   }
 });
