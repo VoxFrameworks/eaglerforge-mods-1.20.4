d1204ea3cc2024bb5c00ab84d79ae4037dcedf55
VoxFrameworks <vdmofficially@outlook.com>
2026-02-13 16:50:38 -0500

    Add network module for fall damage prevention

--- pulse-jump.js
+++ pulse-jump.js
@@ -1,7 +1,9 @@
 ModAPI.require("player");
+ModAPI.require("network"); // for fall damage prevention
 ModAPI.addEventListener("key", (event) => {
   if (event.key === 35) { // Key code for 'h'
     ModAPI.player.motionY += 1;
+      ModAPI.network.sendPacketPlayer({isOnGround: true}); // prevent fall damage
     ModAPI.updateComponent("player");
   }
 });
