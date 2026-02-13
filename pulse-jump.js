ff2e9abaf6a85f7b17a0e91840e0cf5269c8df24
VoxFrameworks <vdmofficially@outlook.com>
2026-02-13 16:57:51 -0500

    Refactor fall damage prevention logic

--- pulse-jump.js
+++ pulse-jump.js
@@ -3,9 +3,11 @@ ModAPI.require("network"); // for fall damage prevention
 ModAPI.addEventListener("key", (event) => {
   if (event.key === 35) { // Key code for 'h'
     ModAPI.player.motionY += 1;
-  if (ModAPI.player.fallDistance > 2.0) {
-      ModAPI.network.sendPacketPlayer({isOnGround: true}); // prevent fall damage
-       ModAPI.player.fallDistance = 0; 
+     ModAPI.player.fallDistance = 0;
+     ModAPI.network.sendPacketPlayer({
+            isOnGround: true
+        }); // prevent fall damage
+      
   } 
     ModAPI.updateComponent("player");
   }
