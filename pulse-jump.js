ee59c1f110b5f119cc0deaada6cfc349c674fb72
VoxFrameworks <vdmofficially@outlook.com>
2026-02-13 16:47:34 -0500

    Implement pulse jump on key press

---
+++ pulse-jump.js
@@ -0,0 +1,7 @@
+ModAPI.require("player");
+ModAPI.addEventListener("key", (event) => {
+  if (event.key === 35) { // Key code for 'h'
+    ModAPI.player.motionY += 1;
+    ModAPI.updateComponent("player");
+  }
+});
